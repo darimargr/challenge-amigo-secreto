1d08389bd98267646cca22de6956517d3d151255
Darimar Garcia <darimargr@gmail.com>
2025-09-21 18:05:03 -0300

    Modificaciones para utilizar el programa varias veces sin tener que actualizar la página

--- app.js
+++ app.js
@@ -9,12 +9,8 @@ function actualizarArrayAmigos(nombre){
   let existe = false;
 
   //Obtener el elemento de la lista  
-  const listaAmigos = document.getElementById('listaAmigos');
-
-  //Limpiar la lista existente
-  //listaAmigos.innerHTML = "";
-
-  //Iterar sobre el arreglo para validar que no exista el nombre
+  const listaAmigos = document.getElementById('listaAmigos'); 
+  //Validar que no exista el nombre en la lista
   for (let i = 0; i < amigos.length; i++) {
      if (amigos.length !== 0 && amigos[i] == nombre) {
        alert(`Ya el nombre ${nombre} existe en la lista`); 
@@ -22,7 +18,7 @@ function actualizarArrayAmigos(nombre){
        break;        
      } 
   }
-  // Agregar elemento a la lista y al arreglo.
+  // Agregar elemento a listaAmigos y al arreglo amigos.
   if (existe == false) {
     //Agregar elemento a la lista
     const item = document.createElement('li'); 
@@ -33,36 +29,47 @@ function actualizarArrayAmigos(nombre){
   }
 }
 
-function agregarAmigo(){
- 
+//Inicializa el elemento resultado
+function inicializarResultado(){
+  if (resultado.length !== 0){
+      resultado.innerHTML = "";
+      resultado.textContent = "";
+    }
+}
+
+function agregarAmigo(){ 
   // Capturar el campo de entrada y quitar los espacios en blanco
   const nombreAmigo = campoNombre.value.trim(); 
-
-  //Validar la entrada
-  if (nombreAmigo == "") {                     // Si el campo está en blanco envía un alert
+  //Validar la entrada no esté en blanco
+  if (nombreAmigo == "") {                     
     alert("Por favor, inserte un nombre");
   } else {
-    //Actualizar el array amigos
+  //Actualizar el array amigos
     actualizarArrayAmigos(nombreAmigo);
-
-    // Limpiar el campo de entrada
+  // Limpiar el campo de entrada y resultado (cuando sea necesario) 
     campoNombre.value = "";
-    campoNombre.focus();   
+    campoNombre.focus(); 
+    inicializarResultado();
   }            
 }
 
 function sortearAmigo(){
-  const resultado = document.getElementById('resultado');
- 
+  const resultado = document.getElementById('resultado'); 
   //Validar que la lista amigos tenga algún elemento
-  if (amigos.length !== 0) {
-    //Determinar el amigo aleatoriamente
-    let amigoElegido = parseInt(Math.random() * amigos.length);   
+  if (amigos.length !== 0) {   
+    //Determinar el amigo secreto aleatoriamente
+    let amigoElegido = parseInt(Math.random() * amigos.length);  
+    //Limpiar la lista existente
+    listaAmigos.innerHTML = "";   
     //Mostrar el amigo elegido
     resultado.textContent = `El amigo secreto es ${amigos[amigoElegido]}`;
-
+    //Limpia el arreglo amigos para una nueva jugada
+    amigos.length = 0;
   } else {
-    alert("Por favor, inserte los nombres de los amigos");
+    //Si el arreglo amigos está vacío orienta al usuario para que agregue a los amigos
+    alert("Si desea seguir jugando debe insertar el nombre de los amigos");
+    campoNombre.focus(); 
+    inicializarResultado();
   }
 }
 
